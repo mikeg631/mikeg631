39c8b60516fd9a99003fbcc9f53516d230500596
mikeg631 <mikeg631@ufl.edu>
2020-08-27 14:27:25 -0400

    Converted recursion to iterative approach

--- Senior Project/Server/processingServer/processingServer/NiftiTransform.py
+++ Senior Project/Server/processingServer/processingServer/NiftiTransform.py
@@ -2,23 +2,32 @@ import os
 import numpy as np
 import nibabel as nib
 
-def loop_access(n,m,data,tpl):
-    if n >m:
-        return loop_access(n,m+1,data[tpl[m]],tpl)
-    else:
-        return data[tpl[m]]
+def makeMap(mapCoords,dims,data):
+    n = len(dims)
+    m = 0
+    tempTple = ()
+    while n > m:
+        tempTple = (tempTple+(0,))
+        m += 1
+    m = 0
+    while n > m:
+        #do stuff with temp tuple
+        temp = data
+        l = 0
+        while n > l:
+            temp = temp[tempTple[l]]
+            l += 1
 
-def loop_rec(n,m,mapCoords,dims,data,tple):
-    if n >= m:
-        for x in range(dims[m]):
-            loop_rec(n,m+1,mapCoords,dims,data,(tple+(x,)))
-    else:
-        temp = loop_access(len(dims)-1,0,data,tple)#recurse to find array element
-        print(temp)
         if temp in mapCoords:
-            mapCoords[temp].append(tple)# add coord to list
+            mapCoords[temp].append(tempTple)# add coord to list
         else:
             mapCoords[temp] = list() #make list, it doesn't exiss:
+            mapCoords[temp].append(tempTple)
+
+        if tempTple[m] >= dims[m]:
+            m += 1
+        else:
+            tempTple[m] += 1
 
 def transform(dataFile,save_path):
     img = nib.load(dataFile)
@@ -32,7 +41,7 @@ def transform(dataFile,save_path):
     #mapcoords is passed to store the values to, dims in the shape of the data, data is passed
     # () is an empty tuple, this is added to during each recursion 
     dims = data.shape()
-    loop_rec(len(dims)-1,0,mapCoords,dims,data,()) 
+    makeMap(mapCoords,dims,data) 
 
     for key in mapCoords:
         tList = mapCoords[key]
